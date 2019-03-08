8e604301e77b5430d174c8923cc642a28ed5f467
17kibr <17kibr@stockholmscience.se>
2019-03-08 13:20:24 +0100

    KianCommit
    kiki

--- Program.cs
+++ Program.cs
@@ -10,15 +10,52 @@ namespace ConsoleLibrary
     {
         static void Main(string[] args)
         {
-            
+            Start();
         }
         static void Start()
         {
+            string TemporaryString;
             Console.WriteLine("What do you want to do?");
             Console.WriteLine("ST :Search tile");
             Console.WriteLine("SA :Search author");
             Console.WriteLine("A :Show all books");
-            Console.WriteLine("");
+            Console.Write(">>");
+            TemporaryString = Console.ReadLine();
+            switch (TemporaryString)
+            {
+                case "ST":
+                case "st":
+                    SearchTitle();
+                    break;
+                case "SA":
+                case "sa":
+                    SearchAuthor();
+                    break;
+                case "A":
+                case "a":
+                    ShowAll();
+                    break;
+                default:
+                    Console.WriteLine("Please only write ST,st,SA,sa,A or a");
+                    Start();
+                    break;
+            }
+        }
+        static void SearchTitle()
+        {
+            Console.WriteLine("Enter the book's name:");
+            Console.Write(">>");
+            string TemporaryString = Console.ReadLine();
+        }
+        static void SearchAuthor()
+        {
+            Console.WriteLine("Enter the author's name:");
+            Console.Write(">>");
+            string TemporaryString = Console.ReadLine();
+        }
+        static void ShowAll()
+        {
+
         }
     }
 }
