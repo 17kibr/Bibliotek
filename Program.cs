e37143cbf828682f5aa2c86eb895d728ac1437ae
17kibr <17kibr@stockholmscience.se>
2019-03-08 12:47:30 +0100

    First commit
    First

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        static void Start()
+        {
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("ST :Search tile");
+            Console.WriteLine("SA :Search author");
+            Console.WriteLine("A :Show all books");
+            Console.WriteLine("");
+        }
+    }
+}
